e46dd3f1801cc0f036af3d3dd3cb42d03e5ef4a4
Riel526 <gabpadolina05@gmail.com>
2024-04-29 07:55:07 +0800

    Created model, factory, and seeded into phpmyadmin

--- database/migrations/2024_04_28_232704_create_invoice_items_table.php
+++ database/migrations/2024_04_28_232704_create_invoice_items_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('counters', function (Blueprint $table) {
+        Schema::create('invoice_items', function (Blueprint $table) {
             $table->id();
-            $table->string('key')->unique();
-            $table->string('prefix');
-            $table->string('value');
+            $table->integer('invoiceId');
+            $table->integer('productId');
+            $table->double('unitPrice');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('counters');
+        Schema::dropIfExists('invoice_items');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Counter;
+use App\Models\Product;
+use App\Models\Customer;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        Counter::factory(1)->create();
+        Product::factory(5)->create();
+        Customer::factory(5)->create(); 
+        Invoice::factory(5)->create(); 
+        InvoiceItem::factory(5)->create(); 
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
+
 }
